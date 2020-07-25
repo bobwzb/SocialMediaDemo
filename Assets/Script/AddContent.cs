9c531c14f614bb09d15e8a5e4e438efdf4943451
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-07-25 02:54:34 -0700

    fix bug for load

--- Assets/Script/AddContent.cs
+++ Assets/Script/AddContent.cs
@@ -6,11 +6,38 @@ using LitJson;
 public class AddContent : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string jsonData;
+    public JsonData json;
+    public List<PostInfo> postList;
+    public List<GameObject> posts;
     void Start()
     {
-        
+        postList = new List<PostInfo>();
+        ParseJson();
+        createObject();
     }
+    void ParseJson()
+    {
+        TextAsset itemText = Resources.Load<TextAsset>("Post");
+        json = JsonMapper.ToObject(itemText.text);
+        jsonData = JsonMapper.ToJson(json);
+        foreach (JsonData tmp in json)
+        {
+            string id = "h" + tmp["id"].ToString();
+            string name= tmp["Name"].ToString();
+            string image = tmp["image"].ToString();
+            string text= tmp["text"].ToString();
+            PostInfo Info = new PostInfo(id, name, text, image);
+            postList.Add(Info);
+        }
+    }
+    void createObject()
+    {
+        for(int i = 0; i < posts.Count; i++)
+        {
 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Script/PostInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostInfo 
+{
+    // Start is called before the first frame update
+    public string id { get; set; }
+    public string Name { get; set; }
+    public string text { get; set; }
+    public string image { get; set; }
+    void Start()
+    {
+        
+    }
+    public PostInfo(string id,string name,string text,string image)
+    {
+        this.id = id;
+        this.Name = name;
+        this.text = text;
+        this.image = image;
+    }
+}
